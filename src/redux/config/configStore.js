9631e436a453b6521073f4ffa041097dc47f4010
강지현 <kangjihyeon@gangjihyeon-ui-MacBookAir.local>
2024-05-24 21:14:50 +0900

    redux 폴더 생성 및 store 생성

---
+++ src/redux/config/configStore.js
@@ -0,0 +1,11 @@
+import { createStore } from 'redux';
+import { combineReducers } from 'redux';
+
+// 1) rootReducer 만들기
+const rootReducer = combineReducers({});
+
+// 2) store 조합
+const store = createStore(rootReducer);
+
+// 3) 만든 store 내보내기
+export default store;
