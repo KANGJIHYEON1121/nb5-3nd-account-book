cad9806149dc81b66813cb957cf197a46f4da03f
강지현 <kangjihyeon@gangjihyeon-ui-MacBookAir.local>
2024-05-24 15:48:22 +0900

    props drilling 코드 작성

--- src/pages/DetailedExpense.jsx
+++ src/pages/DetailedExpense.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { useLocation, useNavigate, useParams } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 
 const ExpenseList = styled.li`
