7c8101337ab49fe44c7111f80beec4417027e759
강지현 <kangjihyeon@gangjihyeon-ui-MacBookAir.local>
2024-05-27 10:48:24 +0900

    코드 수정

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -4,7 +4,7 @@ import { combineReducers } from 'redux';
 // 1) rootReducer 만들기
 const rootReducer = combineReducers({});
 
-// 2) store 조합
+// 2) store 생성
 const store = createStore(rootReducer);
 
 // 3) 만든 store 내보내기
